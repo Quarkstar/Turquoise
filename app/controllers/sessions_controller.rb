767c8c0c8aced5304e31c5cf463c06bbe5e817ae
Nzyzzz <17373101@buaa.edu.cn>
2019-12-11 10:24:09 +0800

    User Finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      sign_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_back_or @user
     else
       flash.now[:danger] = 'Invalid email/password'
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    sign_out
+    sign_out if signed_in?
     redirect_to root_url
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,26 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def remember(user)
+        user.remember
+        cookies.permanent.signed[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: session[:user_id])
+        elsif (user_id = cookies.signed[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                sign_in user
+                @current_user = user
+            end
+        end
+    end
+
+    def current_user?(user)
+        user == current_user
     end
 
     def signed_in?
@@ -13,8 +31,24 @@ module SessionsHelper
     end
 
     def sign_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
+    end
+
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
     end
 
 end
